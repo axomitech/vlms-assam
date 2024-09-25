6d75ccfa1e34a300c69739904f6a6cb78cb1bb75
Ashish Agarwal <stormchaser0123@gmail.com>
2024-09-25 17:13:02 +0530

    AdminModel Query Update

--- app/Models/AdminModel.php
+++ app/Models/AdminModel.php
@@ -30,6 +30,7 @@ class AdminModel extends Model
                 'departments.id as dept_id',
                 'user_departments.id as profile_id',
                 'user_departments.default_access as default_access',
+                'user_departments.first_receiver'
             )
             ->where('user_departments.user_id', '!=', Auth::user()->id)
             ->get();
